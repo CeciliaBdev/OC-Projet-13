d2494678148d927a0bfb9848980a94171740c73d
CeciliaBdev <cecilia.bernard@live.fr>
2022-11-14 15:39:46 +0100

    debut fonction welcome user

---
+++ front/src/Pages/User.js
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Name from '../components/NameLogged'
+
+function User() {
+  return (
+    <div>
+      <Header />
+      <Name />
+      <Footer />
+    </div>
+  )
+}
+
+export default User
---
+++ front/src/components/NameLogged.js
@@ -0,0 +1,16 @@
+function Name() {
+  return (
+    <div className=" flex flex-col h-[100vh] bg-[#13072B]  text-white font-bold pt-8 items-center gap-2">
+      <p className="text-2xl">Welcome back</p>
+      <p className="text-2xl">User</p>
+      <button
+        type="button"
+        className="bg-[#00bc77] p-2 w-24 text-sm font-light border "
+      >
+        Edit Name
+      </button>
+    </div>
+  )
+}
+
+export default Name
